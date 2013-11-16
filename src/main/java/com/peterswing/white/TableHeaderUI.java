ffc39c581aa378445f446940c796aa9c2160e17f
Peter <mcheung63@hotmail.com>
2013-11-16 19:39:08 +0800

    Signed-off-by: Peter <mcheung63@hotmail.com>

--- src/main/java/com/peterswing/white/TableHeaderUI.java
+++ src/main/java/com/peterswing/white/TableHeaderUI.java
@@ -5,23 +5,34 @@ import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
 import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+import javax.swing.event.MouseInputListener;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicTableHeaderUI;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 
 public class TableHeaderUI extends BasicTableHeaderUI {
+	boolean mouseOver;
+	int mouseOverColumn = -1;
+
 	Image normalBG = new ImageIcon(TableHeaderUI.class.getResource("images/PTable/PTable_Header.png")).getImage();
 	Image mouseOverBG = new ImageIcon(TableHeaderUI.class.getResource("images/PTable/PTable_Header_mouseOver.png")).getImage();
 
 	public static ComponentUI createUI(JComponent h) {
 		return new TableHeaderUI();
 	}
+
+	//	protected MouseInputListener createMouseInputListener() {
+	//		super.createMouseInputListener();
+	//		return new MouseInputHandler();
+	//	}
 
 	public void paint(Graphics g, JComponent c) {
 		Random rand = new Random();
@@ -68,4 +79,49 @@ public class TableHeaderUI extends BasicTableHeaderUI {
 		return null;
 	}
 
+//	class MouseInputHandler implements MouseInputListener {
+//
+//		public void mouseEntered(MouseEvent e) {
+//			mouseOver = true;
+//			TableColumnModel columnModel = header.getColumnModel();
+//			mouseOverColumn = columnModel.getColumnIndexAtX(e.getPoint().x);
+//			header.repaint();
+//		}
+//
+//		public void mouseExited(MouseEvent e) {
+//			mouseOver = false;
+//			header.repaint();
+//		}
+//
+//		@Override
+//		public void mouseClicked(MouseEvent e) {
+//			// TODO Auto-generated method stub
+//
+//		}
+//
+//		@Override
+//		public void mousePressed(MouseEvent e) {
+//			// TODO Auto-generated method stub
+//
+//		}
+//
+//		@Override
+//		public void mouseReleased(MouseEvent e) {
+//			// TODO Auto-generated method stub
+//
+//		}
+//
+//		@Override
+//		public void mouseDragged(MouseEvent e) {
+//			// TODO Auto-generated method stub
+//
+//		}
+//
+//		@Override
+//		public void mouseMoved(MouseEvent e) {
+//			// TODO Auto-generated method stub
+//
+//		}
+//
+//	}
 }
